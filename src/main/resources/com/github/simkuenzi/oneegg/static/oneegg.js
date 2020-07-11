26146d3831eaaaea97a343cc0a70b0fccce36a35
Simon Künzi <sim.kuenzi@gmail.com>
2020-07-11 11:06:55 +0200

    Keep selected option

--- src/main/resources/com/github/simkuenzi/oneegg/static/oneegg.js
+++ src/main/resources/com/github/simkuenzi/oneegg/static/oneegg.js
@@ -1,8 +1,13 @@
 function updateReferenceNames() {
     $.post("evalRef", $("#ingredients-in").val(), function(data) {
+        var selected = $("#reference-name option:selected").text();
         $('#reference-name').empty();
         data.forEach(function(item, index) {
-            $('#reference-name').append($("<option></option>").attr("value", index).text(item));
+            var option = $("<option></option>").attr("value", index).text(item);
+            if (item == selected) {
+                option = option.attr("selected", true);
+            }
+            $('#reference-name').append(option);
         });
     });
 }
