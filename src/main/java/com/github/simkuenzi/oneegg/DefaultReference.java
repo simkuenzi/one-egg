98a21f4a5cf3008d117c08aca3d540e35574bdf6
Simon Künzi <sim.kuenzi@gmail.com>
2020-07-11 12:31:11 +0200

    Populate reference-type drop down

--- src/main/java/com/github/simkuenzi/oneegg/DefaultReference.java
+++ src/main/java/com/github/simkuenzi/oneegg/DefaultReference.java
@@ -2,18 +2,18 @@ package com.github.simkuenzi.oneegg;
 
 public class DefaultReference {
     private final String productName;
-    private final ReferenceType scalar;
+    private final ReferenceType referenceType;
 
-    public DefaultReference(String productName, ReferenceType scalar) {
+    public DefaultReference(String productName, ReferenceType referenceType) {
         this.productName = productName;
-        this.scalar = scalar;
+        this.referenceType = referenceType;
     }
 
     public String getProductName() {
         return productName;
     }
 
-    public ReferenceType getScalar() {
-        return scalar;
+    public ReferenceType getReferenceType() {
+        return referenceType;
     }
 }
--- src/main/resources/com/github/simkuenzi/oneegg/static/oneegg.js
+++ src/main/resources/com/github/simkuenzi/oneegg/static/oneegg.js
@@ -17,6 +17,16 @@ function updateReferenceNames() {
         if (!hasSelection && $("#reference-name option").length > 0) {
             $.post("evalDef", $("#ingredients-in").val(), function(data) {
                 $("#reference-name option:contains('" + data.productName + "')").attr("selected", true);
+                $("#reference-type").empty();
+                switch (data.referenceType) {
+                    case 'SCALAR':
+                        $("#reference-type").append($("<option value='exactly'>exactly</option>"));
+                    break;
+                    case 'RANGE':
+                        $("#reference-type").append($("<option value='at-least'>at least</option>"));
+                        $("#reference-type").append($("<option value='at-most'>at most</option>"));
+                    break;
+                }
             });
         }
     });
